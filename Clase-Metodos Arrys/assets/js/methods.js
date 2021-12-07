be2c39692b97da3fb10ec2900b870be6653b24f8
Alessandra Baltazar <abaltazar@estudiantes.crackthecode.la>
2021-12-07 10:25:23 -0500

    Otros metodos de arrys

--- Clase-Metodos Arrys/assets/js/methods.js
+++ Clase-Metodos Arrys/assets/js/methods.js
@@ -1,44 +1,73 @@
 //Variables: Array y div
 var names = ["EXO","Blackpink","TXT","Mamamoo"];
 var content = document.getElementById("array-content");
+var newContent = document.getElementById("new-arrays-content");
 
 //Moostar datos del array al cragar pagina
-showData();
+showData(names,content);
 
 //Variables de botones
 let btnPush = document.getElementById("btn-Push");
 let btnPop = document.getElementById("btn-Pop");
 let btnUnshift = document.getElementById("btn-Unshift");
 let btnShift = document.getElementById("btn-Shift");
+let btnSlice = document.getElementById("btn-Slice");
+let btnConcat = document.getElementById("btn-Concat");
+let btnSplice = document.getElementById("btn-Splice");
 
 //Anclar funciones a los botones
 btnPush.onclick = fPush;
 btnPop.onclick = fPop;
 btnUnshift.onclick = fUnshift;
 btnShift.onclick = fShift;
+btnSlice.onclick = fSlice; 
+btnConcat.onclick = fConcat;
+btnSplice.onclick = fSplice;
 
 //Funciones
 function fPush(e){
     names.push("BTS");
-    showData();
+    showData(names,content);
 }
 
 function fPop(e){
     names.pop();
-    showData();
+    showData(names,content);
 }
 function fUnshift(e){
     names.unshift("SuperJunior");
-    showData();
+    showData(names,content);
 }
 function fShift(e){
     names.shift();
-    showData();
+    showData(names,content);
 }
-function showData(){ 
+
+function fSlice(e){
+    b = 0
+    e = 2
+    let newArray = names.slice(b,e);
+    showData(newArray, newContent);
+}
+
+function fConcat(e){
+    colorsArray = ["Rosa","Negro","Dorado"]
+    mergeArray = names.concat(colorsArray);
+    showData(mergeArray,newContent)
+}
+
+function fSplice(e){
+    b = 1;
+    c = 3;
+    newString = "Juanito";
+    names.splice(b,c,newString);
+    showData(names,newContent);
+}
+
+function showData(array,div){ 
     let lst="";
-    for(i=0;i < names.length;i++){
-        lst += "<p class='item'>"+names[i]+"</p>";
+    for(i=0;i < array.length;i++){
+        lst += "<p class='item'>"+array[i]+"</p>";
     }
-    content.innerHTML=lst;
+    div.innerHTML=lst;
 }
