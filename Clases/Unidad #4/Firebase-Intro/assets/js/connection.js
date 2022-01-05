0c7bc584f50e379caed4faeb303ff05fc3109e54
Alessandra Baltazar <abaltazar@estudiantes.crackthecode.la>
2022-01-05 08:40:34 -0500

    Nuevas carpetas y mini proyecto nuevo de gastronomia

---
+++ Clases/Unidad #4/Firebase-Intro/assets/js/connection.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+    $("#registro").hide();
+
+    $("#btn-register").click(function(){
+        $("#login-container").hide();
+        $("#registro").show();
+    })
+    // La configuración de Firebase de su aplicación web
+    // Para Firebase JS SDK v7.20.0 y versiones posteriores, MeasureId es opcional
+    const firebaseConfig = {
+        apiKey: "AIzaSyCs_IhKdb_gE0XjkzURoGlnJBtf79Ah2mU",
+        authDomain: "proyecto-bootcamp-unicef-27f07.firebaseapp.com",
+        projectId: "proyecto-bootcamp-unicef-27f07",
+        storageBucket: "proyecto-bootcamp-unicef-27f07.appspot.com",
+        messagingSenderId: "495616354191",
+        appId: "1:495616354191:web:d24318a7b073e8f4938dfa",
+        measurementId: "G-3RYMKGBGJF"
+    };
+
+    //Inicializar Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    //Inicializar servicio de autentificacion
+    const auth = firebase.auth();
+
+    //Login o inicio de sesion
+    $("#btn-login").click(function(e){
+        e.preventDefault();
+        //Variables de inputs
+        var correo = $("#mail").val();
+        var clave = $("#pass").val();
+        //Usar servicio de login de firebase
+        auth.signInWithEmailAndPassword(correo,clave)
+        .then(userCredential=>{
+            alert("Datos Correctos");
+        })
+        .catch((error) =>{
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            alert("Código: "+errorCode+ ". Mensaje: "+errorMessage);
+        }) 
+    })
+
+    //Singup o crear cuenta
+    $("#btn-singup").click(function(e){
+        e.preventDefault();
+        //Variables de inputs
+        var correo = $("#mail-new").val();
+        var clave = $("#pass-new").val();
+        //Usar servicio de firebase para crear cuenta
+        auth.createUserWithEmailAndPassword(correo,clave)
+        .then(userCredential=>{
+            $("#login-container").show();
+            $("#registro").hide();
+            alert("Cuenta creada");
+        })
+        .catch((error)=>{
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            alert("Código: "+errorCode+". Mensaje: "+errorMessage);
+        })
+    })
+    //Desconexion de usuario
+    //Boton LogOut
+    $("#logout").click(function(e){
+        e.preventDefault();
+        auth.signOut().then(() => {
+            console.log("Log Out");
+        })
+    })
+
+    auth.onAuthStateChanged((user)=>{
+        if(user){
+            //Sesion Iniciada
+            $("#login-container").hide();
+        }
+        else{
+            //Sesion finalizada
+            $("#login-container").show();
+        }
+    })
+
+})
+
